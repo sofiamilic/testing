58850cd5c8da911fe6ab930e592fea9df098595f
Renato Van de Wyngard <renatovandewyngard@200-200-83-eduroam-al.wlan.net.uc.cl>
2023-08-30 15:10:21 -0400

    Assertion Less Tests implemented

--- T1/core/rules/assertion_less.py
+++ T1/core/rules/assertion_less.py
@@ -4,13 +4,21 @@ from ..rule import *
 class AssertionLessVisitor(WarningNodeVisitor):
     # Implementar Clase
     def __init__(self):
-        pass
+        super().__init__()
+
+    def visit_FunctionDef(self, node):
+        code = dump(node)
+        if "assert" not in code:
+            self.addWarning("AssertionLessWarning", node.lineno, "it is an assertion less test")
+        self.generic_visit(node)
 
 
 class AssertionLessTestRule(Rule):
     #  Implementar Clase
     def analyze(self, node):
-        pass
+        visitor = AssertionLessVisitor()
+        visitor.visit(node)
+        return visitor.warningsList()
 
         
     @classmethod
