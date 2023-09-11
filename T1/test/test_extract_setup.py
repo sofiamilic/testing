485633284126d6b7a3376813e85e5a45fe0e4938
Renato Van de Wyngard <renatovandewyngard@MacBook-Air-de-Renato.local>
2023-09-10 21:50:23 -0300

    solve border case extract_setup

--- T1/test/test_extract_setup.py
+++ T1/test/test_extract_setup.py
@@ -29,8 +29,6 @@ class TestX(TestCase):
         self.assertEquals(self.x,self.y)
     def test_y(self):
         self.assertEquals(self.y,self.x)"""
-        print("should be")
-        print(dump(parse(code), indent=4))
 
         self.assertAST(result,
                        """
@@ -108,6 +106,116 @@ class TestX(TestCase):
         p = Person("Maria", "21")
         self.assertEquals(p.name(),"Maria")""")
 
+
+    def test_extract_setup_3(self):
+        result = rewrite(ExtractSetupCommand,
+                         """
+class TestX(TestCase):
+    def test_x(self):
+        x = 2
+        for i in range(10):
+            x = x + 1
+        self.assertEquals(x,12)
+    def test_y(self):
+        x = 2
+        for i in range(10):
+            x = x + 1
+        self.assertEquals(12,x)""")
+
+        code = """
+class TestX(TestCase):
+    def setUp(self):
+        self.x = 2
+        for i in range(10):
+            self.x = self.x + 1
+    def test_x(self):
+        self.assertEquals(self.x,12)
+    def test_y(self):
+        self.assertEquals(12,self.x)"""
+
+        print(result)
+        self.assertAST(result,code)
+
+    def test_extract_setup_4(self):
+        result = rewrite(ExtractSetupCommand,
+                         """
+class TestX(TestCase):
+    def test_x(self):
+        x = Person("Juan", "?")
+        x.age = 2
+        self.assertEquals(x.age,12)
+    def test_y(self):
+        x = Person("Juan", "?")
+        x.age = 2
+        self.assertEquals(12,x.age)""")
+
+        code = """
+class TestX(TestCase):
+    def setUp(self):
+        self.x = Person("Juan", "?")
+        self.x.age = 2
+    def test_x(self):
+        self.assertEquals(self.x.age,12)
+    def test_y(self):
+        self.assertEquals(12,self.x.age)"""
+
+        print(result)
+        self.assertAST(result,code)
+
+    def test_extract_setup_3(self):
+        result = rewrite(ExtractSetupCommand,
+                         """
+class TestX(TestCase):
+    def test_x(self):
+        x = 2
+        for i in range(10):
+            x = x + 1
+        self.assertEquals(x,12)
+    def test_y(self):
+        x = 2
+        for i in range(10):
+            x = x + 1
+        self.assertEquals(12,x)""")
+
+        code = """
+class TestX(TestCase):
+    def setUp(self):
+        self.x = 2
+        for i in range(10):
+            self.x = self.x + 1
+    def test_x(self):
+        self.assertEquals(self.x,12)
+    def test_y(self):
+        self.assertEquals(12,self.x)"""
+
+        print(result)
+        self.assertAST(result,code)
+
+    def test_extract_setup_5(self):
+        result = rewrite(ExtractSetupCommand,
+                         """
+class TestX(TestCase):
+    def test_x(self):
+        x = Person("Juan", "?")
+        create_person(x,2)
+        self.assertEquals(x.age,12)
+    def test_y(self):
+        x = Person("Juan", "?")
+        create_person(x,2)
+        self.assertEquals(12,x.age)""")
+
+        code = """
+class TestX(TestCase):
+    def setUp(self):
+        self.x = Person("Juan", "?")
+        create_person(self.x,2)
+    def test_x(self):
+        self.assertEquals(self.x.age,12)
+    def test_y(self):
+        self.assertEquals(12,self.x.age)"""
+
+        print(result)
+        self.assertAST(result,code)
 
 if __name__ == '__main__':
     unittest.main()
